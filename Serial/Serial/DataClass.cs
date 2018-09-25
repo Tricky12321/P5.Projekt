2adf62e288c6b57557354b85170aa3da4b2da657
Tricky12321 <Thue-V-Iversen@hotmail.com>
2018-09-25 14:13:04 +0200

    Fix

---
+++ Serial/Serial/DataClass.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+namespace Serial
+{
+	public class DataClass
+	{
+		private const int StackSize = 100;
+        public Int64 X;
+        public Int64 Y;
+        public Int64 Z;
+
+        private Queue<int> X_Stack = new Queue<int>();
+        private Queue<int> Y_Stack = new Queue<int>();
+        private Queue<int> Z_Stack = new Queue<int>();
+        
+		private double _hz_Rate = 0;
+		private double _hz = 0;
+		private object _hzLock = new object();
+		public DataClass()
+		{
+			for (int i = 0; i < StackSize; i++)
+			{
+				X_Stack.Enqueue(0);
+				Y_Stack.Enqueue(0);
+				Z_Stack.Enqueue(0);
+			}
+			Thread HzThread = new Thread(CalculateHz);
+			HzThread.Start();
+
+		}
+
+		public void UpdateX(int XVal)
+		{
+			X_Stack.Enqueue(XVal);
+			X_Stack.Dequeue();
+		}
+
+		public void UpdateY(int YVal)
+		{
+			Y_Stack.Enqueue(YVal);
+			Y_Stack.Dequeue();
+		}
+
+		public void UpdateZ(int ZVal)
+		{
+			Z_Stack.Enqueue(ZVal);
+			Z_Stack.Dequeue();
+		}
+
+		public int GetX()
+		{
+			return Convert.ToInt32(Math.Round(X_Stack.Average(), 0));
+
+		}
+
+		public int GetY()
+		{
+			return Convert.ToInt32(Math.Round(Y_Stack.Average(), 0));
+
+		}
+
+		public int GetZ()
+		{
+			return Convert.ToInt32(Math.Round(Z_Stack.Average(), 0));
+		}
+
+		public void NewNum() {
+			lock (_hzLock) {
+				_hz++;
+            }
+		}
+
+		private void CalculateHz() {
+			Thread.Sleep(1000);
+			_hz_Rate = _hz;
+            lock (_hzLock)
+            {
+				_hz = 0;
+			}
+			CalculateHz();
+		}
+
+		public double GetHz() {
+			return _hz_Rate;
+		}
+	}
+}
---
+++ Serial/Serial/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.IO.Ports;
+using System.Threading;
+namespace Serial
+{
+    class MainClass
+    {
+
+		static bool _continue = true;
+        static SerialPort _serialPort;
+		static Thread readThread = new Thread(Read);
+        public static void Main()
+        {
+            string message;
+            StringComparer stringComparer = StringComparer.OrdinalIgnoreCase;
+
+
+
+			ConnectToCom();
+            Console.WriteLine("Type QUIT to exit");
+
+            while (_continue)
+            {
+                message = Console.ReadLine();
+                if (stringComparer.Equals("quit", message))
+                {
+                    _continue = false;
+                }
+            }
+
+            _serialPort.Close();
+        }
+
+		public static void ConnectToCom() {
+			
+
+			// Create a new SerialPort object with default settings.
+            _serialPort = new SerialPort("/dev/cu.usbmodem1411", 115200);
+
+            // Allow the user to set the appropriate properties.
+
+            // Set the read/write timeouts
+            _serialPort.ReadTimeout = 500;
+            _serialPort.WriteTimeout = 500;
+
+            _serialPort.Open();
+            _continue = true;
+            readThread.Start();
+			readThread.Join();
+            
+		}
+
+        public static void Read()
+        {
+			DataClass Accelerometer = new DataClass();
+            while (_continue)
+            {
+                try
+                {
+                    string message = _serialPort.ReadLine();
+					if (message.Contains("AC") && message.Contains(":")) {
+						message = message.Substring(2, message.Length - 3);
+
+                        var message_split = message.Split(':');
+                        int X = Convert.ToInt32(message_split[0]);
+                        int Y = Convert.ToInt32(message_split[1]);
+                        int Z = Convert.ToInt32(message_split[2]);
+						Accelerometer.NewNum();
+						//Accelerometer.UpdateX(X);
+						//Accelerometer.UpdateY(Y);
+						//Accelerometer.UpdateZ(Z);
+                        Console.Clear();
+                        Console.WriteLine($"X: {X}");
+                        Console.WriteLine($"Y: {Y}");
+                        Console.WriteLine($"Z: {Z}");
+						Console.WriteLine($"FILTER");
+						//Console.WriteLine($"X: {Accelerometer.GetX()}");
+						//Console.WriteLine($"Y: {Accelerometer.GetY()}");
+						//Console.WriteLine($"Z: {Accelerometer.GetZ()}");
+						Console.WriteLine($"HZ: {Accelerometer.GetHz()}");
+					}
+                }
+                catch (TimeoutException) { }
+				catch (FormatException) {
+					Console.Clear();
+					Console.WriteLine("Data error");
+				
+				}
+            }
+        }
+
+
+    }
+}
