6dacc6cf7cfd5a9772557512d210bd537f886809
Unknown <simon9400@live.dk>
2018-10-08 15:05:19 +0200

    Begun pozyx_reader class
    Not done, does not work

---
+++ Serial/Serial/pozyx_reader.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serial
+{
+    class PozyxReader
+    {
+        private static SerialPort _serialPort;
+        public PozyxReader()
+        {
+            _serialPort = new SerialPort();
+            string[] portNames = SerialPort.GetPortNames();
+            while (true)
+            {
+                foreach (string portName in portNames)
+                {
+                    Console.WriteLine(portName);
+                    _serialPort.PortName = portName;
+                    _serialPort.Open();
+                    _serialPort.Write("69");//error here
+                    _serialPort.Close();
+                }
+            }
+        }
+
+        public XYZ getPosition()
+        {
+            XYZ position = new XYZ();
+            return position;
+        }
+    }
+}
