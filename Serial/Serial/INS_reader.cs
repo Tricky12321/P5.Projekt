f684568c791cc2fe28b7db90d46f90e3fb7f31c5
Lasse Østergaard <last16@student.aau.dk>
2018-10-10 14:14:11 +0200

    Getting hotter

--- Serial/Serial/INS_reader.cs
+++ Serial/Serial/INS_reader.cs
@@ -10,6 +10,13 @@ namespace Serial
     class INS_reader : IReadable<INSDATA>
     {
         private SerialPort _serialPort;
+        private string _dataSet;
+        private double XGY;
+        private double YGY;
+        private double ZGY;
+        private double XAC;
+        private double YAC;
+        private double ZAC;
 
         public INS_reader()
         {
@@ -22,7 +29,44 @@ namespace Serial
 
         public INSDATA Read()
         {
-            Console.WriteLine(_serialPort.ReadLine());
+            _dataSet = _serialPort.ReadLine();
+            string data2 = _serialPort.ReadLine();
+            try
+            {
+                if (_dataSet.Contains("GY") && _dataSet.Contains(":"))
+                {
+                    _dataSet = _dataSet.Substring(2, _dataSet.Length - 3);
+
+                    var message_split = _dataSet.Split(':');
+                    XGY = Convert.ToDouble(message_split[0]);
+                    YGY = Convert.ToDouble(message_split[1]);
+                    ZGY = Convert.ToDouble(message_split[2]);
+
+                }
+            }
+            catch (TimeoutException) { }
+            catch (FormatException) { }
+            catch (IndexOutOfRangeException) { }
+
+            try
+            {
+                if (_dataSet.Contains("AC") && _dataSet.Contains(":"))
+                {
+                    _dataSet = _dataSet.Substring(2, _dataSet.Length - 3);
+
+                    var message_split = _dataSet.Split(':');
+                    XAC = Convert.ToDouble(message_split[0]);
+                    YAC = Convert.ToDouble(message_split[1]);
+                    ZAC = Convert.ToDouble(message_split[2]);
+
+                }
+            }
+            catch (TimeoutException) { }
+            catch (FormatException) { }
+            catch (IndexOutOfRangeException) { }
+
+            _dataSet += "test" + _serialPort.ReadLine();
+            Console.WriteLine(_dataSet);
             return new INSDATA();
         }
     }
