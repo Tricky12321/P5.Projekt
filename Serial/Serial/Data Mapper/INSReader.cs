5ad52ff91b08dd3a8c1488ffa5cfb3089e150ea4
Mads Stenkær <mstenk16@student.aau.dk>
2018-10-15 15:01:23 +0200

    Har lavet lidt struktur i det der serial lort

--- Serial/Serial/Data Mapper/INSReader.cs
+++ Serial/Serial/Data Mapper/INSReader.cs
@@ -4,8 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO.Ports;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
 using System.Collections.Concurrent;
 
--- Serial/Serial/Program.cs
+++ Serial/Serial/Program.cs
@@ -4,15 +4,14 @@ using System.Threading;
 using System.Diagnostics;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
+using NeuralNetwork;
 
 namespace Serial
 {
 	class MainClass
-	{
-
+	{ 
 		static bool _continue = true;
 		static SerialPort _serialPort;
-		//static Thread readThread = new Thread(Read);
 		static Stopwatch DataTimer = new Stopwatch();
 		static DataClass Accelerometer = new DataClass("AC");
 		static DataClass Gyroscope = new DataClass("GY");
@@ -21,37 +20,36 @@ namespace Serial
 		static PositionCalculator positionCalculator = new PositionCalculator();
 
 
-		static INSReader iNS_Reader;
+        static INSReader insReader = new INSReader();
 		public static void Main()
 		{
-			/*
-            Console.WriteLine($"Creating POZYX.");
-            PozyxReader Pozyx = new PozyxReader();
-            Console.WriteLine($"Reading POZYX.");
+            insReader = new INSReader();
+            PozyxReader posReader = new PozyxReader();
+            DataMapper dataMapper = new DataMapper(posReader, insReader);
 
-            SerialPort Test = SerialReader.GetSerialPort(ArduinoTypes.POZYX);
-			Console.WriteLine($"Serialport found: {Test.PortName}");
-            */
-			iNS_Reader = new INSReader();
+            dataMapper.StartReading();
+            Thread.Sleep(1000);
+            var list = dataMapper.ReadToList(100);
+            dataMapper.StopReading();
+
 			Thread PrintThread = new Thread(Print);
 			PrintThread.Start();
 			PrintThread.Join();
 			Console.ReadLine();
-
 		}
 
 		public static void Print()
 		{
 			while (true)
 			{
-				iNS_Reader.Read();
+				insReader.Read();
 				Console.Clear();
 				Console.WriteLine($"INS READER");
 				Console.WriteLine($"Aceelerometer data");
-				Console.WriteLine(iNS_Reader.AcceXYZ);
+				Console.WriteLine(insReader.AcceXYZ);
 				Console.WriteLine($"Gyro data");
-				Console.WriteLine(iNS_Reader.GyroXYZ);
-				Console.WriteLine($"HZ:{iNS_Reader.HZ_rate}");
+				Console.WriteLine(insReader.GyroXYZ);
+				Console.WriteLine($"HZ:{insReader.HZ_rate}");
 			}
 		}
 	}
