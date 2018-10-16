4ca8123210d45060feda407424d2070cdd07f2cc
Mads Stenkær <mstenk16@student.aau.dk>
2018-10-16 09:18:50 +0200

    Er ved at implementere serial i nn

---
+++ Serial/Serial/Neural Network/INS_POSZYX_NeuralNetworkTester.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+
+namespace NeuralNetwork
+{
+    public class INS_POSZYX_NeuralNetworkTester
+    {
+        public INSReader insReader = new INSReader();
+        public PozyxReader posReader = new PozyxReader();
+
+        public INS_POSZYX_NeuralNetworkTester()
+        {
+        }
+
+        public void Start(){
+
+            insReader = new INSReader();
+            PozyxReader posReader = new PozyxReader();
+            DataMapper dataMapper = new DataMapper(posReader, insReader);
+
+            dataMapper.StartReading();
+            Thread.Sleep(1000);
+            var list = dataMapper.ReadToList(100);
+            dataMapper.StopReading();
+
+            Thread PrintThread = new Thread(Print);
+            PrintThread.Start();
+            PrintThread.Join();
+            Console.ReadLine();
+        }
+
+        public static void Print()
+        {
+            while (true)
+            {
+                insReader.Read();
+                Console.Clear();
+                Console.WriteLine($"INS READER");
+                Console.WriteLine($"Aceelerometer data");
+                Console.WriteLine(insReader.AcceXYZ);
+                Console.WriteLine($"Gyro data");
+                Console.WriteLine(insReader.GyroXYZ);
+                Console.WriteLine($"HZ:{insReader.HZ_rate}");
+            }
+        }
+    }
+}
