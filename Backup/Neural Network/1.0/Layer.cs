eca5458f07539b041f141b9595cad5ca253df507
Thue Iversen <Thue-V-Iversen@hotmail.com>
2018-11-15 11:13:14 +0100

    Tilføjet Backup mappen af NN

---
+++ Backup/Neural Network/1.0/Layer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+namespace NeuralNetwork1
+{
+    [Serializable]
+    public class Layer
+    {
+        public List<Neuron> Neurons;
+        public int NeuronCount
+        {
+            get
+            {
+                return Neurons.Count;
+            }
+        }
+        public Layer(int numNeurons)
+        {
+            Neurons = new List<Neuron>(numNeurons);
+        }
+    }
+}
---
+++ Backup/Neural Network/2.0/Layer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NeuralNetwork2
+{
+    [Serializable]
+    public class Layer
+    {
+        public List<Neuron> Neurons = new List<Neuron>();
+
+        public double[] ToArray()
+        {
+            double[] valueArray = new double[Neurons.Count];
+            for (int neuronCount = 0; neuronCount < Neurons.Count; neuronCount++)
+            {
+                valueArray[neuronCount] = Neurons[neuronCount].Value;
+            }
+            return valueArray;
+        }
+    }
+}
