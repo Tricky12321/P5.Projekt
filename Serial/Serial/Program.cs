4ef0b2cde7dd99bff49e4e3321b90053c608a090
Thue Iversen <Thue-V-Iversen@hotmail.com>
2018-11-27 14:43:16 +0100

    Lavet så alle filer er med punktum istedet for comma

--- Serial/Serial/Program.cs
+++ Serial/Serial/Program.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using Serial.Menu;
+using System.Threading;
+using System.Globalization;
+
 namespace Serial
 {
     class MainClass
     {
         public static void Main()
         {
+			Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture("en-GB");
 			MainMenu.ShowMenu();
         }
     }
