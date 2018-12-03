925dedd0c0c25618e0f5acc4d7ee9da10b8f752c
Mads Stenkær <mstenk16@student.aau.dk>
2018-12-03 13:43:58 +0100

    Lidt ekstra

--- Serial/Serial/Program.cs
+++ Serial/Serial/Program.cs
@@ -6,14 +6,28 @@ using System.Linq;
 using System.IO;
 using System.Threading;
 using System.Globalization;
+using weka.clusterers;
+using weka.core;
+using java.io;
+
 namespace Serial
 {
     class MainClass
     {
         public static void Main()
         {
-			      Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture("en-US");
-			      MainMenu.ShowMenu();
+            //var tsfdsljk = System.Environment.CurrentDirectory;
+
+            var test = new FileReader("/Users/stenkaer/Documents/GitHub/P5.Projekt/Serial/Serial/bin/Debug/Test/iris-kopi.arff");
+            Instances instances = new Instances(test);
+            EM em = new EM();
+            em.buildClusterer(instances);
+
+            var sfjhdks = em.getClass();
+            var sasasasas = em.toString();
+
+            Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture("en-US");
+			MainMenu.ShowMenu();
         }
     }
 }
